4bde53651894dceefe854036b5cc0d06b3044d96
magwyen <akpinarmurat@protonmail.com>
2023-04-13 17:11:28 +0000

    first commit

---
+++ systemcheck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Check for any broken packages
+echo -e "Checking for broken packages..."
+if [ $(dpkg -l | grep "^..r" | wc -l) -ne 0 ]; then
+    echo -e "${RED}There are broken packages on the system.${NC}"
+else
+    echo -e "${GREEN}No broken packages found.${NC}"
+fi
+
+# Check for any updates that have not been installed
+echo -e "Checking for available updates..."
+if [ $(sudo apt list --upgradable --no-upgrade 2>/dev/null | wc -l) -ne 0 ]; then
+    echo -e "${RED}There are updates available that have not been installed.${NC}"
+else
+    echo -e "${GREEN}No updates available.${NC}"
+fi
+
+# Check for any errors in the system log
+echo -e "Checking system log for errors..."
+if [ $(sudo grep -i error /var/log/syslog /var/log/dmesg | wc -l) -ne 0 ]; then
+    echo -e "${RED}There are errors in the system log.${NC}"
+else
+    echo -e "${GREEN}No errors found in the system log.${NC}"
+fi
+
+echo -e "${GREEN}System check complete.${NC}"
+
